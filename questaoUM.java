0350eb92889a1a9a30485d3b0c0c5e6ff4acba29
Adriano Dias <98924782+AdrianoPno@users.noreply.github.com>
2022-02-20 23:41:55 -0300

    questão 1

---
+++ questaoUM.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class questaoUM {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		Scanner entrada = new Scanner(System.in);
+        
+		System.out.println("Digite o numero de degraus:");
+		int numero = entrada.nextInt();
+		
+        
+        for (int i=1; i<= numero; i++) {
+            
+            for (int j=numero-i; j >= 1; j--)
+                System.out.print(" ");
+                for (int jw =1; jw <= i; jw++)
+                System.out.print("*");
+                System.out.println(" ");
+        entrada.close();
+        }
+    }
+}
